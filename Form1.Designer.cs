a80ab89da71a25bdbd29fc7a483ede739a4b722e
MalakAlyazji <161591417+MalakAlyazji@users.noreply.github.com>
2024-03-09 20:30:28 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿
+namespace CardFlipGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbchoice1 = new System.Windows.Forms.Label();
+            this.lbchoice2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbround = new System.Windows.Forms.Label();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btgenrate = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbturn = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Olive;
+            this.label1.Location = new System.Drawing.Point(37, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Choice 1 :";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Olive;
+            this.label2.Location = new System.Drawing.Point(37, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 35);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Choice 2 :";
+            // 
+            // lbchoice1
+            // 
+            this.lbchoice1.AutoSize = true;
+            this.lbchoice1.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbchoice1.Location = new System.Drawing.Point(182, 51);
+            this.lbchoice1.Name = "lbchoice1";
+            this.lbchoice1.Size = new System.Drawing.Size(27, 29);
+            this.lbchoice1.TabIndex = 2;
+            this.lbchoice1.Text = "?";
+            // 
+            // lbchoice2
+            // 
+            this.lbchoice2.AutoSize = true;
+            this.lbchoice2.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbchoice2.Location = new System.Drawing.Point(182, 139);
+            this.lbchoice2.Name = "lbchoice2";
+            this.lbchoice2.Size = new System.Drawing.Size(27, 29);
+            this.lbchoice2.TabIndex = 3;
+            this.lbchoice2.Text = "?";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Olive;
+            this.label3.Location = new System.Drawing.Point(37, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 28);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Round:";
+            // 
+            // lbround
+            // 
+            this.lbround.AutoSize = true;
+            this.lbround.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbround.Location = new System.Drawing.Point(144, 250);
+            this.lbround.Name = "lbround";
+            this.lbround.Size = new System.Drawing.Size(28, 29);
+            this.lbround.TabIndex = 6;
+            this.lbround.Text = "1";
+            // 
+            // button12
+            // 
+            this.button12.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button12.BackgroundImage")));
+            this.button12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button12.Location = new System.Drawing.Point(868, 462);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(87, 77);
+            this.button12.TabIndex = 12;
+            this.button12.Tag = "54";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button_Click_1);
+            // 
+            // button11
+            // 
+            this.button11.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button11.BackgroundImage")));
+            this.button11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button11.Location = new System.Drawing.Point(687, 462);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(87, 77);
+            this.button11.TabIndex = 11;
+            this.button11.Tag = "72";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button_Click_1);
+            // 
+            // button10
+            // 
+            this.button10.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button10.BackgroundImage")));
+            this.button10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button10.Location = new System.Drawing.Point(514, 462);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(87, 77);
+            this.button10.TabIndex = 10;
+            this.button10.Tag = "72";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button_Click_1);
+            // 
+            // button9
+            // 
+            this.button9.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button9.BackgroundImage")));
+            this.button9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button9.Location = new System.Drawing.Point(345, 462);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(87, 77);
+            this.button9.TabIndex = 9;
+            this.button9.Tag = "29";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button_Click_1);
+            // 
+            // button8
+            // 
+            this.button8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button8.BackgroundImage")));
+            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button8.Location = new System.Drawing.Point(868, 295);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(87, 77);
+            this.button8.TabIndex = 8;
+            this.button8.Tag = "86";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button_Click_1);
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button7.BackgroundImage")));
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button7.Location = new System.Drawing.Point(687, 295);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(87, 77);
+            this.button7.TabIndex = 7;
+            this.button7.Tag = "13";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button_Click_1);
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button6.Location = new System.Drawing.Point(514, 295);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(87, 77);
+            this.button6.TabIndex = 6;
+            this.button6.Tag = "86";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button5.Location = new System.Drawing.Point(345, 295);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(87, 77);
+            this.button5.TabIndex = 5;
+            this.button5.Tag = "99";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button4.Location = new System.Drawing.Point(868, 139);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 77);
+            this.button4.TabIndex = 4;
+            this.button4.Tag = "99";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.Location = new System.Drawing.Point(687, 139);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 77);
+            this.button3.TabIndex = 3;
+            this.button3.Tag = "54";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Location = new System.Drawing.Point(514, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 77);
+            this.button2.TabIndex = 2;
+            this.button2.Tag = "29";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(345, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 77);
+            this.button1.TabIndex = 1;
+            this.button1.Tag = "13";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_Click_1);
+            // 
+            // btgenrate
+            // 
+            this.btgenrate.BackColor = System.Drawing.Color.Khaki;
+            this.btgenrate.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btgenrate.ForeColor = System.Drawing.Color.Maroon;
+            this.btgenrate.Location = new System.Drawing.Point(32, 351);
+            this.btgenrate.Name = "btgenrate";
+            this.btgenrate.Size = new System.Drawing.Size(216, 105);
+            this.btgenrate.TabIndex = 13;
+            this.btgenrate.Text = "Generate";
+            this.btgenrate.UseVisualStyleBackColor = false;
+            this.btgenrate.Click += new System.EventHandler(this.btgenerate_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label4.Font = new System.Drawing.Font("Tahoma", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Olive;
+            this.label4.Location = new System.Drawing.Point(418, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(314, 58);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Card Flip Game";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label5.Font = new System.Drawing.Font("Tahoma", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(793, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(117, 52);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Turn:";
+            // 
+            // lbturn
+            // 
+            this.lbturn.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbturn.ForeColor = System.Drawing.Color.DarkKhaki;
+            this.lbturn.Location = new System.Drawing.Point(905, 37);
+            this.lbturn.Name = "lbturn";
+            this.lbturn.Size = new System.Drawing.Size(81, 34);
+            this.lbturn.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1013, 600);
+            this.Controls.Add(this.lbturn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btgenrate);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lbround);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbchoice2);
+            this.Controls.Add(this.lbchoice1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Card Flip Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbchoice1;
+        private System.Windows.Forms.Label lbchoice2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbround;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button btgenrate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbturn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace CardFlipGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        enum enchoice { choice1,choice2}
+        enchoice choice = enchoice.choice1;
+        void changepicture(Button bt)
+        {
+            if (bt.Tag.ToString() == "13")
+            {
+                bt.Image = Image.FromFile(@"C:\Users\hp\Downloads\13.jpg");
+                
+            }else if (bt.Tag.ToString() == "29")
+            {
+                bt.Image = Image.FromFile(@"C:\Users\hp\Downloads\29.jpg");
+            }
+            else if (bt.Tag.ToString() == "54")
+            {
+                bt.Image = Image.FromFile(@"C:\Users\hp\Downloads\54.jpg");
+            }
+            else if (bt.Tag.ToString() == "72")
+            {
+                bt.Image = Image.FromFile(@"C:\Users\hp\Downloads\72.jpg");
+            }
+            else if (bt.Tag.ToString() == "86")
+            {
+                bt.Image = Image.FromFile(@"C:\Users\hp\Downloads\86.jpg");
+            }
+            else
+            {
+                bt.Image = Image.FromFile(@"C:\Users\hp\Downloads\99.jpg");
+            }
+            bt.Enabled = false;
+        }
+        void checkround(Button bt1,Button bt2)
+        { 
+            if (bt1.Tag!=bt2.Tag )
+            {
+                bt1.Image = Image.FromFile(@"C:\Users\hp\Downloads\questionmark.jpg");
+                bt2.Image = Image.FromFile(@"C:\Users\hp\Downloads\questionmark.jpg");
+                bt2.Enabled = true;
+                bt1.Enabled = true;
+            }
+            else
+            {
+                count++;
+            }
+            
+        }
+        public Button b1;
+        int round = 1;
+        int count = 0;
+        void playround(Button bt)
+        {
+            switch (choice)
+                {
+                    case enchoice.choice1:
+                         b1=bt;
+                         lbchoice2.Text = "?";
+                         lbturn.Text = "Ch1";
+                         lbchoice1.Text = bt.Tag.ToString();
+                        choice = enchoice.choice2;
+                        changepicture(bt);
+                        break;
+                    case enchoice.choice2:
+                        lbchoice2.Text = bt.Tag.ToString();
+                        choice = enchoice.choice1;
+                        changepicture(bt);
+                        lbturn.Text = "Ch2";
+                    Thread.Sleep(200);
+                        checkround(b1, bt);
+                    break;
+                }
+            
+        }
+        private void button_Click_1(object sender, EventArgs e)
+        {
+            playround((Button)sender);
+        }
+
+        private void btgenerate_Click(object sender, EventArgs e)
+        {
+            if (count == 6)
+            {
+                count = 0;
+                MessageBox.Show("Great Work! All Done :)\n press generate to play again", "Done ", MessageBoxButtons.OK);
+                round++;
+                lbchoice1.Text = "?";
+                lbchoice2.Text = "?";
+                lbround.Text = round.ToString();
+                button1.Image = Image.FromFile(@"C:\Users\hp\Downloads\questionmark.jpg");
+                button2.Image = Image.FromFile(@"C:\Users\hp\Downloads\questionmark.jpg");
+                button3.Image = Image.FromFile(@"C:\Users\hp\Downloads\questionmark.jpg");
+                button4.Image = Image.FromFile(@"C:\Users\hp\Downloads\questionmark.jpg");
+                button5.Image = Image.FromFile(@"C:\Users\hp\Downloads\questionmark.jpg");
+                button6.Image = Image.FromFile(@"C:\Users\hp\Downloads\questionmark.jpg");
+                button7.Image = Image.FromFile(@"C:\Users\hp\Downloads\questionmark.jpg");
+                button8.Image = Image.FromFile(@"C:\Users\hp\Downloads\questionmark.jpg");
+                button9.Image = Image.FromFile(@"C:\Users\hp\Downloads\questionmark.jpg");
+                button10.Image = Image.FromFile(@"C:\Users\hp\Downloads\questionmark.jpg");
+                button11.Image = Image.FromFile(@"C:\Users\hp\Downloads\questionmark.jpg");
+                button12.Image = Image.FromFile(@"C:\Users\hp\Downloads\questionmark.jpg");
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+                button6.Enabled = true;
+                button7.Enabled = true;
+                button8.Enabled = true;
+                button9.Enabled = true;
+                button10.Enabled = true;
+                button11.Enabled = true;
+                button12.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Sorry , you cant start a new game without flipping any card", "Error ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
